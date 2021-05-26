6359b7faa77fcf3468a269a2a5908b93e1f5545d
JWallbanks <jdwallbanks@aol.com>
2021-05-26 08:19:00 +0100

    Added Unit testing project

---
+++ WebAPI/WebAPI.UnitTests/Services/CsvToModelServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using WebAPI.Services;
+
+namespace WebAPI.UnitTests.Services
+{
+    [TestFixture]
+    public class CsvToModelServiceTests
+    {
+        private CsvToModelService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new CsvToModelService();
+        }
+
+
+
+        [Test]
+        public void Method_Scenario_ExpectedResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
